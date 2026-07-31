52ff2cbe3df7e4bc845d9ac58289fdc0343d15ec
G. Sumner Hayes <shayes@sumnerhayes.org>
2026-07-31 15:47:58 -0400

    Split off presets into new python file

--- fireworks_app.py
+++ fireworks_app.py
@@ -17,6 +17,7 @@ import OpenGL.contextdata
 # Mock PyOpenGL context
 OpenGL.contextdata.getContext = lambda context=None: 1
 
+from presets import active_presets
 from constants import (
     RARITY_INTERVAL,
     COLORS,
@@ -68,98 +69,8 @@ class FireworksApp:
         self.opt_color_mode = 'REALISTIC' # 'REALISTIC', 'NEON', 'TRANQUIL', 'METAL'
         self.opt_height_restrict = True
         self.mandala_slices = 12
+        self.active_presets = active_presets
         
-        self.active_presets = [
-            {
-                "name": "Fire Plasma",
-                "major_mode": "FIRE Plasma",
-                "show_rockets": True,
-                "opt_color_mode": "REALISTIC",
-                "opt_trailers": 6,
-                "opt_gravity": 1.0,
-                "opt_height_restrict": True,
-                "opt_star_shape": 0
-            },
-            {
-                "name": "Fireworks",
-                "major_mode": "FIREWORKS",
-                "show_rockets": True,
-                "opt_color_mode": "REALISTIC",
-                "opt_trailers": 0,
-                "opt_gravity": 1.0,
-                "opt_height_restrict": True,
-                "opt_star_shape": 0
-            },
-            {
-                "name": "Glory",
-                "major_mode": "FIREWORKS",
-                "show_rockets": False,
-                "opt_color_mode": "NEON",
-                "opt_trailers": 10,
-                "opt_gravity": 0.0,
-                "opt_height_restrict": False,
-                "opt_star_shape": 2 # small diamonds
-            },
-            {
-                "name": "Wormhole",
-                "major_mode": "TUNNEL Wormhole",
-                "show_rockets": True,
-                "opt_color_mode": "REALISTIC",
-                "opt_trailers": 0,
-                "opt_gravity": 1.0,
-                "opt_height_restrict": True,
-                "opt_star_shape": 0
-            },
-            {
-                "name": "Mandala",
-                "major_mode": "MANDALA Sacred",
-                "show_rockets": True,
-                "opt_color_mode": "REALISTIC",
-                "opt_trailers": 0,
-                "opt_gravity": 1.0,
-                "opt_height_restrict": True,
-                "opt_star_shape": 0,
-                "mandala_slices": 12
-            },
-            {
-                "name": "Trance",
-                "major_mode": "MANDALA Sacred",
-                "show_rockets": True,
-                "opt_color_mode": "TRANQUIL",
-                "opt_trailers": 10,
-                "opt_gravity": 0.5,
-                "opt_height_restrict": False,
-                "opt_star_shape": 2, # larger diamonds
-                "mandala_slices": 4
-            },
-            {
-                "name": "Underwater",
-                "major_mode": "UNDERWATER Lava",
-                "show_rockets": True,
-                "opt_color_mode": "REALISTIC",
-                "opt_trailers": 0,
-                "opt_gravity": 1.0,
-                "opt_height_restrict": True,
-                "opt_star_shape": 0
-            },
-            {
-                "name": "Synaesthesia",
-                "major_mode": "SYNAESTHESIA Classic",
-                "show_rockets": True,
-                "opt_color_mode": "REALISTIC",
-                "opt_trailers": 0,
-                "opt_gravity": 1.0,
-                "opt_height_restrict": True,
-                "opt_star_shape": 5, # shape star (5 points)
-                "syn_star_size": 0.25,
-                "syn_fade_mode": "Wave"
-            },
-            {
-                "name": "Random",
-                "major_mode": None,
-                "random_preset": True
-            }
-        ]
         self.preset_idx = 0
         self.preset_random_mode = random_mode
         self.preset_random_timer = 0.0
